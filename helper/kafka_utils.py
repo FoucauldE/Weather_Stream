2d08b404c1eaad8233ebd77305ba186d7b2ed8f9
FoucauldE <foucauld.estignard@edu.devinci.fr>
2025-01-11 17:16:05 +0100

    deleted useless code

--- helper/kafka_utils.py
+++ helper/kafka_utils.py
@@ -2,8 +2,6 @@ from kafka import KafkaProducer, KafkaConsumer
 import json
 from config.config import KAFKA_BROKER
 
-# KAFKA_TOPIC = 'weather_data'
-
 def create_kafka_producer():
     producer = KafkaProducer(
         bootstrap_servers=KAFKA_BROKER,
@@ -27,18 +25,3 @@ def create_kafka_consumer(input_topic):
 def consume_messages_from_kafka(consumer):
     for message in consumer:
         yield message.value
-
-
-
-if __name__ == "__main__":
-    # Example: Sending a sample message to Kafka
-    producer = create_kafka_producer()
-    if producer:
-        message = {"location": "New York", "temperature": 22, "humidity": 60}
-        send_message_to_kafka(producer, message)
-    
-    # Example: Consuming messages from Kafka
-    consumer = create_kafka_consumer()
-    if consumer:
-        for msg in consume_messages_from_kafka(consumer):
-            print("Consumed message:", msg)
