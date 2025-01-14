77aa71a831f1a92fb729e670b770a02dae0580ac
Nathan Tm <n22toumi@gmail.com>
2025-01-14 02:17:17 +0100

    Still to tune

--- scripts/batch_model.py
+++ scripts/batch_model.py
@@ -3,21 +3,27 @@ import pandas as pd
 from config.config import TARGET_VARIABLE, CITIES_LIST
 import time
 from river import compose, linear_model, metrics, preprocessing
+from river.tree import HoeffdingTreeRegressor
 from kafka import KafkaConsumer
 import json
 
 
 def main():
     # Kafka configuration
-    KAFKA_TOPIC_TRAIN = "weather_train"
-    KAFKA_TOPIC_TEST = "weather_test"
+    KAFKA_TOPIC_TRAIN = "weather_train_with_Y"
+    KAFKA_TOPIC_TEST = "weather_test_with_Y"
     KAFKA_BOOTSTRAP_SERVER = "localhost:9092"
 
     # Variables
     MAX_MESSAGES_TRAIN = int(len(CITIES_LIST) * 24 * 8 * 0.8)
     MAX_MESSAGES_TEST = int(len(CITIES_LIST) * 24 * 8 * 0.2)
 
-    # Loading the data
+    # Load the data
+    data = pd.read_csv("Data/all_weather_data_with_Y.csv")
+
+    # Split
+    #train_data = data.iloc[:int(0.8 * len(data))]
+    #test_data = data.iloc[int(0.8 * len(data)):-1]
     train_data = pd.read_csv("Data/train_data_historical.csv")
     test_data = pd.read_csv("Data/test_data_historical.csv")
 
@@ -37,7 +43,8 @@ def main():
     # River for online learning
     model = compose.Pipeline(
         preprocessing.StandardScaler(),
-        linear_model.LinearRegression()
+        #linear_model.LinearRegression()
+        HoeffdingTreeRegressor()
     )
     metric = metrics.R2()
 
@@ -48,7 +55,6 @@ def main():
     #consumer_test = create_kafka_consumer(KAFKA_TOPIC_TEST, auto_offset_reset='earliest')
 
     # Train
-    last_precip = None
     i = 0
     print("Training...")
     for message in consumer_train:
@@ -57,46 +63,39 @@ def main():
         data_point = message.value
 
         # Prepare features and target
-        features = {key: value for key, value in data_point.items() if key != TARGET_VARIABLE}
-        current_precip = data_point[TARGET_VARIABLE]
-
-        if last_precip is not None and features is not None:
-            # Train the model
-            model.learn_one(features, last_precip)
-            print(i)
-
+        features = {key: value for key, value in data_point.items() if key != "y_target"}
+        target = data_point['y_target']
+        print(target)
+        # Train the model
+        model.learn_one(features, target)
+        print(i)
         # Update the loop variables
-        last_precip = current_precip
         i += 1
 
     # Test
-    last_precip = None
     print("Test...")
     i = 0
-
     for message in consumer_test:
         if i >= MAX_MESSAGES_TEST:  # Stop after the max number of iterations is reached
             break
         data_point = message.value
-
         print(i)
 
         # Prepare features and target
-        features = {key: value for key, value in data_point.items() if key != TARGET_VARIABLE}
-        current_precip = data_point[TARGET_VARIABLE]
+        features = {key: value for key, value in data_point.items() if key != "y_target"}
+        target = data_point["y_target"]
 
-        if last_precip is not None:
-            # Predict the next value of 'precip_mm'
-            prediction = model.predict_one(features)
-            print("YES",i)
-            # Evaluate the performance
-            metric.update(current_precip, prediction)
-            print(f"Actual value: {current_precip:.2f}, Prediction: {prediction:.2f}, R²: {metric.get():.4f}")
+        # Predict the next value of 'precip_mm'
+        prediction = model.predict_one(features)
+        print("YES",i)
+
+        # Evaluate the performance
+        metric.update(target, prediction)
+        print(f"Actual value: {target:.2f}, Prediction: {prediction:.2f}, R²: {metric.get():.4f}")
 
         # Update the loop variables
-        last_precip = current_precip
         i += 1
-        
+
     print("End of the testing part.")
     print(f"Final R²: {metric.get():.4f}")
 
