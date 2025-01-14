a6ea36641c42c2aefb9a407bf27d33ebb9d81062
Nathan Tm <n22toumi@gmail.com>
2025-01-14 01:35:31 +0100

    Before adapting to new preprocess

--- scripts/get_batch_data.py
+++ scripts/get_batch_data.py
@@ -50,6 +50,24 @@ def get_data(start_date, end_date, output_csv_path):
             date += timedelta(days=1)
     print("Data collected")
 
+def format_csv(csv_path="Data/all_weather_data.csv", prediction_distance=1):
+    """
+    Input: csv file
+    Output: the same but with the target added at the end of each line
+    """
+    # Charger le fichier CSV
+    df = pd.read_csv(csv_path)
+
+    # Ajouter la dernière colonne de la ligne suivante à la ligne actuelle
+    df['y_target'] = df['precip_mm'].shift(-1)
+    df = df.iloc[:-1]
+
+    # Sauvegarder dans un nouveau fichier CSV
+    file_path = csv_path
+    base_directory = os.path.dirname(csv_path)
+    output_file_path = os.path.join(base_directory, 'all_weather_data_with_Y.csv')
+    df.to_csv(output_file_path, index=False)
+
 def train_test_csv(file_path):
     # Get the file
     file_path = file_path
@@ -60,6 +78,6 @@ def train_test_csv(file_path):
     # Output file paths
     train_file_path = os.path.join(base_directory, 'train_data_historical.csv')
     test_file_path = os.path.join(base_directory, 'test_data_historical.csv')
-    # # Save the outputs
+    # Save the outputs
     train_data.to_csv(train_file_path, index=False)
     test_data.to_csv(test_file_path, index=False)
