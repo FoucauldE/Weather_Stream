3aca7e5bba5354706fa6beb94f3d489798e38b81
FoucauldE <foucauld.estignard@edu.devinci.fr>
2025-01-15 11:40:59 +0100

    create consumer within train func

--- scripts/online_model.py
+++ scripts/online_model.py
@@ -9,9 +9,9 @@ from river import linear_model, metrics, preprocessing
 
 
 # INPUT_TOPIC = 'weather-live-data'
-input_topic_past_data = 'data-previous-week'
+# input_topic_past_data = 'data-previous-week'
 OUTPUT_TOPIC = 'rain-prediction-output'
-CONSUMER_GROUP = 'rain-prediction-group'
+# CONSUMER_GROUP = 'rain-prediction-group'
 
 # consumer = create_kafka_consumer(INPUT_TOPIC, 'live-weather-consumer-group')
 producer_predictions = create_kafka_producer()
@@ -24,15 +24,15 @@ scaler = preprocessing.StandardScaler()
 
 
 
-def train_and_predict(consumer):
+def train_and_predict():
     """
     Consume messages from Kafka, train the model incrementally, and send predictions.
     """
-
+    consumer = create_kafka_consumer('weather-live-data-2', 'live-data-group')
     previous_timestamp, previous_features, previous_target = None, None, None
 
     for message in consume_messages_from_kafka(consumer):
-        
+        # print('\nmessage:', message)
         timestamp, features, target = message["last_updated"], message["processed_sample"], message["target"]
         
         if previous_features is not None:
@@ -48,10 +48,10 @@ def train_and_predict(consumer):
 
                 send_message_to_kafka(producer_predictions, OUTPUT_TOPIC, {
                     'timestamp': timestamp,
-                    'prediction': y_pred,
+                    'prediction': max(0, y_pred),
                     'actual': target
                 })
-                print(f"Prediction: {y_pred}, Actual: {target}")
+                # print(f"Prediction: {y_pred}, Actual: {target}")
                 
         previous_timestamp, previous_features, previous_target = timestamp, features, target
 
@@ -60,15 +60,15 @@ if __name__ == "__main__":
     print("Starting online learning model...")
     # consumer_preds = create_kafka_consumer('rain-prediction-output')
 
-    consumer_past_data = create_kafka_consumer(input_topic_past_data, 'past-data-group')
-    while True:
-        train_and_predict(consumer_past_data)
+    # consumer_past_data = create_kafka_consumer(input_topic_past_data, 'past-data-group')
+    # while True:
+        # train_and_predict(consumer_past_data)
 
-        """
-        for message in consume_messages_from_kafka(consumer_preds):
-            prediction = message.value
-            print(f"Prediction for next 15min: {prediction['prediction']} mm")
-            print(f"Actual value: {prediction['actual']} mm")
-        """
+    """
+    for message in consume_messages_from_kafka(consumer_preds):
+        prediction = message.value
+        print(f"Prediction for next 15min: {prediction['prediction']} mm")
+        print(f"Actual value: {prediction['actual']} mm")
+    """
 
         # time.sleep(10)
