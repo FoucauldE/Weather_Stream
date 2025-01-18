07218050350ec85aa5d7425ad43ecddfbfeabc34
Nathan Tm <n22toumi@gmail.com>
2025-01-18 17:21:20 +0100

    Page order

--- app/pages/2_🚨_Alerts.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Alerts", page_icon="🚨")
-st.title('🚨 Alerts')
-
-location = st.text_input("Enter City Name or Zip Code", "Brest")
-if st.button("Get Alerted"):
-    pass
